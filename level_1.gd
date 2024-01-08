8da2d44c8374089ad369a4651678c6a6e6a37edb
João Farroba <148582269+joaofarroba@users.noreply.github.com>
2024-01-08 13:49:03 +0000

    Final Details

--- level_1.gd
+++ level_1.gd
@@ -1,16 +1,5 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-
-
-
-
-
 func _on_map_body_entered(body):
 	get_tree().reload_current_scene()
 
